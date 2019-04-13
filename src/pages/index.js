5e7e5509354697df50e95e5107ccb206d2e7f82c
le ma <lcmaheu@gmail.com>
2019-04-13 16:18:23 -0400

    added navbar and HomeHeader corected

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import {HomeHeader} from "../utils"
-// import img from '../images/bcg/homeBcg.jpeg'
+import img from '../images/bcg/homeBcg.jpeg'
 
 const IndexPage = () => ( 
   <Layout>
